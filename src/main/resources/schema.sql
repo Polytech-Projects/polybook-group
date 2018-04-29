1b6283ef04e0328b2426513d78b2803f9d5e9d34
Aliyya Adira <aliyya_aw@yahoo.com>
2018-04-29 16:44:23 +0200

    Table sql completée (primary key + attribut)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,10 @@
-CREATE TABLE NOTE (
-  ID      INT IDENTITY,
-  CONTENT VARCHAR(144)
+CREATE TABLE IF NOT EXISTS NOTE (
+  ID_NOTE INT unsigned NOT NULL AUTO_INCREMENT,
+  CONTENT VARCHAR(144) NOT NULL,
+  AUTEUR VARCHAR (50) NOT NULL,
+  DATE_AJOUT DATETIME NOT NULL,
+  PRIMARY KEY (ID_NOTE)
 );
 
-INSERT INTO NOTE values (1, 'note de la bdd');
-INSERT INTO NOTE values (2, 'note n°2 de la bdd');
+INSERT INTO NOTE values (1, 'note de la bdd', 'Beyonce', NOW() );
+INSERT INTO NOTE values (2, 'note n°2 de la bdd', 'Rihanna', NOW() );
